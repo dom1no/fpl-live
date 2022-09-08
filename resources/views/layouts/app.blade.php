9d5bb2024de9fd328244ad2d0d9cbcbbc1412828
Максим Купров <maxim.kyprov@ylab.io>
2022-09-08 17:25:54 +0300

    fix ssl

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@
 
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+
         <title>{{ config('app.name', 'Argon Dashboard') }}</title>
         <!-- Favicon -->
         <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('img') }}/favicon/apple-touch-icon.png">
