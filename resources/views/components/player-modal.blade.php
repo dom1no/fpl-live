3044b9acec94c6d96bd0a88940546a1e66e8aefb
Максим Купров <maxim.kyprov@ylab.io>
2022-09-10 14:31:53 +0400

    show full name in player modal

--- resources/views/components/player-modal.blade.php
+++ resources/views/components/player-modal.blade.php
@@ -6,7 +6,7 @@
                     <div class="d-flex align-items-center ml--2">
                         <img src="{{ $player->getPhotoUrl() }}" class="player-photo mr-2">
                         <div class="modal-title">
-                            <span class="display-4">{{ $player->name }}</span>
+                            <span class="display-4 lh-120">{{ $player->full_name }}</span>
                             <br>
                             <span class="text-muted text-sm">
                                 {{ $player->team->name }}
