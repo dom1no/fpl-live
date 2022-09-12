ed44fcae39c7494506c65492c9c9578b293c2016
Максим Купров <maxim.kyprov@ylab.io>
2022-09-12 22:37:25 +0300

    add link to auto login

--- app/Notifications/TelegramWelcomeNotification.php
+++ app/Notifications/TelegramWelcomeNotification.php
@@ -4,6 +4,8 @@ namespace App\Notifications;
 
 use App\Models\Manager;
 use Illuminate\Notifications\Notification;
+use MagicLink\Actions\LoginAction;
+use MagicLink\MagicLink;
 use NotificationChannels\Telegram\TelegramMessage;
 
 class TelegramWelcomeNotification extends Notification
@@ -16,6 +18,23 @@ class TelegramWelcomeNotification extends Notification
     public function toTelegram(Manager $manager): TelegramMessage
     {
         return (new TelegramMessage())
-            ->content("Привет, {$manager->name}!");
+            ->content(implode("\n", [
+                $this->getHiText($manager),
+                $this->getInviteLinkText($manager),
+            ]));
+    }
+
+    private function getHiText(Manager $manager): string
+    {
+        return "Привет, {$manager->name}!";
+    }
+
+    private function getInviteLinkText(Manager $manager): string
+    {
+        $action = new LoginAction($manager);
+
+        $urlToAutoLogin = MagicLink::create($action)->url;
+
+        return "Твой профиль готов: [FPL]({{ $urlToAutoLogin }})";
     }
 }
