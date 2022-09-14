7f67f21d70c4e27f0b4b282a88105eabdb7a237f
Максим Купров <maxim.kyprov@ylab.io>
2022-09-14 08:41:57 +0300

    add force reimport players photos

--- app/Console/Commands/ImportPlayersCommand.php
+++ app/Console/Commands/ImportPlayersCommand.php
@@ -18,7 +18,7 @@ class ImportPlayersCommand extends FPLImportCommand
 
     public function signatureArgs(): string
     {
-        return '{--photos}';
+        return '{--photos} {--force}';
     }
 
     // TODO: выгружать данные по травмам, фото, status
@@ -56,7 +56,7 @@ class ImportPlayersCommand extends FPLImportCommand
         $localPhotoName = $player->getFilePhotoName();
 
         $disk = Storage::disk('player-photos');
-        if ($disk->exists($localPhotoName)) {
+        if (!$this->option('force') && $disk->exists($localPhotoName)) {
             return;
         }
 
